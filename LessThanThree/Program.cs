0b9058aa890e8b32ad4c67888e9b3a59f8e5f6dc
Sergey_Podgornov <psv_23@mail.ru>
2022-11-28 21:59:14 +0300

    Добавили программу создающую массив строк, длина которых меньше 3

---
+++ LessThanThree/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+string[] SortArray(string[] array)
+{
+    int k = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+            k++;
+
+    string[] newArray = new string[k];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+}
+
+string[] array = { "Век", "живи", "-", "век", "учись" };
+PrintArray(SortArray(array));
